c0a53bdb6dd33ab8e6318ac71b3069882b410a71
yongpilmoon <m71661@gmail.com>
2023-01-18 13:39:20 +0900

    feat: todo edit, delete 버튼 추가

--- src/features/todo/TodoListItem.tsx
+++ src/features/todo/TodoListItem.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/ui";
 import { useLocation, useNavigate } from "react-router-dom";
 import type { Todo } from "./queries/useTodoList";
+import { AiOutlineEdit, AiOutlineMinusCircle } from "react-icons/ai";
+import colors from "tailwindcss/colors";
 
 function TodoListItem({ title, id }: Todo) {
   const { pathname } = useLocation();
@@ -9,15 +12,27 @@ function TodoListItem({ title, id }: Todo) {
     navigate(`${pathname}?todoId=${id}`);
   };
 
+  const handleEdit = () => {
+    console.log("edit");
+  };
+
   return (
     <ul>
       <li
         role="presentation"
-        className="text-xl p-2"
+        className="flex justify-between items-center text-xl p-2"
         onClick={handleTodoItem}
         onKeyDown={handleTodoItem}
       >
         {title}
+        <div className="grid gap-1 grid-flow-col">
+          <Button variant="text" size="small" onClick={handleEdit}>
+            <AiOutlineEdit size={24} color={colors.gray[600]} />
+          </Button>
+          <Button variant="text" size="small">
+            <AiOutlineMinusCircle size={24} color={colors.red[600]} />
+          </Button>
+        </div>
       </li>
     </ul>
   );
