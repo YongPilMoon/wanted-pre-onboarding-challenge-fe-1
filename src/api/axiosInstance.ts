c08c45ab6b1b4dc112c34445e04bd8d2ce4a17a6
yongpilmoon <m71661@gmail.com>
2023-01-19 15:10:19 +0900

    refactor: api url 환경변수로 분리

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { tokenRepository } from "../utils/token";
 
-const BASE_URL = "http://localhost:8080";
-
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { Authorization: `${tokenRepository.value}` },
 };
 
